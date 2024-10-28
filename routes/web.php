c111a96157cbee951f3da6f4fd233aee9e94e65e
christianLudovic <talchristgmail.com>
2024-10-28 11:48:13 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,18 @@ use App\Http\Controllers\IndexController;
 use App\Http\Middleware\Authenticate;
 use Illuminate\Support\Facades\Route;
 
-/*Route::middleware('guest')->group(function () {
-    
-});*/
+Route::middleware('guest')->group(function () {
+    Route::get('/', [IndexController::class, 'index'])->name('index');
+    Route::get('/auth/{provider}', [AuthController::class, 'redirectToProvider'])->name('auth.redirect');
+    Route::get('/auth/{provider}/callback', [AuthController::class, 'handleProviderCallback']);
+});
 
-Route::get('/', [IndexController::class, 'index'])->name('index');
-Route::get('/auth/{provider}', [AuthController::class, 'redirectToProvider'])->name('auth.redirect');
-Route::get('/auth/{provider}/callback', [AuthController::class, 'handleProviderCallback']);
-
-/*Route::middleware(Authenticate::class)->group(function () {
-    
-});*/
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/settings', function () {
-    return view('settings');
-})->name('settings');
-Route::get('/logout', [AuthController::class, 'logout']);
+Route::middleware(Authenticate::class)->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/settings', function () {
+        return view('settings');
+    })->name('settings');
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
 
 
